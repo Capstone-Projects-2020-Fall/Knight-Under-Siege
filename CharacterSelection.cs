21f2642d6586819c62aa7dccfe7fc00245ea1df0
John Stahl <tug89585@temple.edu>
2020-11-02 18:16:32 -0500

    Added some missing scripts

---
+++ CharacterSelection.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelection : MonoBehaviour
+{
+    public GameObject[] characters;
+    public int selectedCharacter = 0;
+
+    public void NextCharacters()
+    //Takes an array filled with all our character so we can do more than just the planned four
+    //Then uses mod so while the player is cycling through it doesn't create an oout of bounds issue
+    {
+        //Makes the character selected to not being visable in the scence
+        characters[selectedCharacter].SetActive(false);
+
+        //Moves to the next charcter in the array
+        selectedCharacter = (selectedCharacter + 1) % characters.Length;
+
+        //Makes the charcter selected visable in the scene
+        characters[selectedCharacter].SetActive(true);
+    }
+
+    public void PreviousCharacter()
+    //Same thing as the next char, but has the loop go the other way
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if (selectedCharacter < 0)
+        {
+            selectedCharacter += characters.Length;
+        }
+        characters[selectedCharacter].SetActive(true);
+    }
+}
---
+++ PingUI.cs
@@ -0,0 +1,19 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PingUI : MonoBehaviour
+{
+    private Text pingText;
+
+    private void Start()
+    {
+        pingText = this.GetComponent<Text>();
+    }
+    void FixedUpdate()
+    {
+        pingText.text = "" + PhotonNetwork.GetPing() + " ms";
+    }
+}
